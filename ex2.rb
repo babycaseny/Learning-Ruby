ea7c399f91fc7b8a8f4aa28af89db9e178d67d73
Kurisu <neetdetective@Shionjteki-iMac.local>
2014-12-15 22:24:38 +0800

    Exercise 2: Comments and Pound Characters

---
+++ ex2.rb
@@ -0,0 +1,8 @@
+#Learning Ruby ex2
+
+puts "I could have code like this." #and the comment after is ignored
+
+#Use a comment to "disable" or comment out a piece of code:
+#puts "this won't run."
+
+puts "This will run."
