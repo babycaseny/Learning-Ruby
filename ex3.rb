6ef2d44c336f8096f52c369fdf886ad8f2dd5d7d
Sardo Ip <sardo.ip@nxtomo.hk>
2014-12-17 16:22:39 +0800

    Rewrite to using floating point number

--- ex3.rb
+++ ex3.rb
@@ -1,21 +1,36 @@
 puts "I will now count my chickens:"
 
-puts "Hens #{25 + 30 /6} "
-puts "Rooster #{100 -25 * 3 % 4}"
+
+#Count the number of Hens
+puts "Hens #{25.0 + 30.0 /6} "
+
+#Count the number of Rooster
+puts "Rooster #{100.0 -25.0 * 3.0 % 4.0}"
 
 puts "Now I will count the eggs:"
 
+#Count the number of eggs
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+puts 3 + 2 +1 -5 +4 % 2 - 1 / 4 +6
+
 puts "Is it true that 3 +2 < 5 -7 ?"
 
-puts 3 + 2 < 5 - 7
+# 3 + 2 will not less than 5-7, so it will be false
+puts 3.0 + 2.0 < 5.0 - 7.0
 
-puts "What is 3 + 2 ? #{3 + 2}"
-puts "What is 5 - 7 #{5 - 7}"
+#Tell you why it is false ,show 3+5 and 5-7 at string
+puts "What is 3 + 2 ? #{3.0 + 2.0}"
+puts "What is 5 - 7 #{5.0 - 7.0}"
 
 puts "Oh, that's why it's false."
 
 puts "How about some more."
 
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2 }"
-puts "Is it less or equal? #{5 <= -2}"
+#5 is bigger than -2, so it will be true
+puts "Is it greater? #{5.0 > -2.0}"
+
+# 5 not equal -2 , but greater than -2, so it will be true
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+
+#5 is not less than or equal -2 ,so it will be false
+puts "Is it less or equal? #{5.0 <= -2.0}"
