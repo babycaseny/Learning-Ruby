b2d8e64e7f6c42558926730a0d04e653e331a7d4
Sardo Ip <sardo.ip@nxtomo.hk>
2014-12-17 16:46:26 +0800

    More comment and fix mistake

--- ex4.rb
+++ ex4.rb
@@ -6,10 +6,15 @@ passengers = 90 #Number of passengers
 cats_not_driven = cars - drivers #Number of car which are no drivers
 cars_driven = drivers #Number of car which have drivers
 carpool_capacity = cars_driven * space_in_a_car #How many people can be transport today
-average_passengers_per_car = passengers / cars_driven #How many people can be transport by car
+average_passengers_per_car = passengers / cars_driven #How many people can be transport per car
 
+# Show how many cars are there 
 puts "There are #{cars} cars available"
+# Show how many driver are there
 puts "There are only #{drivers} drivers available"
-puts "There will be #{drivers} drivers available."
-puts "We can transport #{carpool_capacity}people today."
+#Show how many empty car are there
+puts "There will be #{cars_not_driven} empty cars today."
+#Show how many people can be transport today
+puts "We can transport #{carpool_capacity} people today."
+#Show how many people can be transport per car
 puts "We need to put about #{average_passengers_per_car} in each car."
