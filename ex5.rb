e6be7653f199701c4196e04616ed6432e6912ef7
Sardo Ip <sardo.ip@nxtomo.hk>
2014-12-17 14:54:36 +0800

    Exercise 5: More Variables and Printing

---
+++ ex5.rb
@@ -0,0 +1,18 @@
+my_name = 'Sardo'
+my_age = 20
+my_height = 142 #cm
+my_weight = 32 #kg
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "She's #{my_height} inches tall."
+puts "She's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "she's got #{my_eyes} eys and #{my_hair} hair."
+puts "Her teeth are usually #{my_eyes} eyes and #{my_hair} hair."
+puts "Her teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add my age, my height, and my weight ,I get #{my_age + my_height + my_weight}."
