dbca4c07628c538119a3dc47001284103390a8bd
Sardo Ip <sardo.ip@nxtomo.hk>
2014-12-17 15:54:17 +0800

    Exercise 6: Strings and Text

---
+++ ex6.rb
@@ -0,0 +1,23 @@
+types_of_people = 10 #Number of type of pepole
+x = "There are #{types_of_people} types of people." #The String of there are how many type of people.
+binary ="binary" #The string of binary
+do_not = "don't" #The string of don't
+y = "Those who know #{binary} and those who #{do_not}." #The String of binary + do_not 
+
+puts x #Show x 
+puts y #Show y
+
+puts "I said:#{x}." #Show x in string(I said:)
+puts "I also said:'#{y}'." #Show y in string(I also said:)
+
+hilarious = false # The boolen of hilarious
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}" #The string which added hilarious
+
+puts joke_evaluation # Show joke_evaluation
+
+w = "This is the left side of ..." #A String of left side
+
+e = "a string with a right side."
+
+#Add the string (w + e)
+puts w + e
