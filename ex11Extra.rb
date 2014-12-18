dca81c2bd676a3579de94a0d6f0d2e08a4b178f8
Sardo Ip <sardo.ip@nxtomo.hk>
2014-12-18 15:05:43 +0800

    Exercise 11: Asking Questions

---
+++ ex11Extra.rb
@@ -0,0 +1,11 @@
+print "What's your name? "
+name = gets.chomp()
+
+print "Are you silly?"
+silly = gets.chomp()
+
+print "Hey!Young man!"
+hey = gets.chomp()
+
+
+puts "#{hey}#{name} you are #{silly} silly ."
