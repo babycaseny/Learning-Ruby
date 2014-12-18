10f43234153d87d8af3bec73a762e8dd5607fba0
Sardo Ip <sardo.ip@nxtomo.hk>
2014-12-18 12:05:53 +0800

    Fix missing

--- ex8.rb
+++ ex8.rb
@@ -3,6 +3,7 @@ formatter = "%s %s %s %s"
 puts formatter % [1,2,3,4]
 puts formatter % ["one","two","three","four"]
 puts formatter % [true, false , false , true]
+puts formatter % [formatter,formatter,formatter,formatter]
 puts formatter % [
     "I had this thing.",
     "That you could type up right.",
