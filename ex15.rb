4d5878ed7a1a729261ce23da45ffe8268c62378d
Sardo Ip <sardo.ip@nxtomo.hk>
2014-12-23 12:07:47 +0800

    Add comments

--- ex15.rb
+++ ex15.rb
@@ -1,15 +1,26 @@
+#Get the filename from user by ARGV.first
 filename = ARGV.first
 
+#Set prompt value
 prompt = ">"
+
+#Use File.open() to open the file by provided filename
 txt = File.open(filename)
 
+#Show filename
 puts "Here's your file: #{filename}"
+
+#Show file content by read()
 puts txt.read()
 
+#Get filename again by user's input
 puts "Type the filename again:"
 print prompt
 file_again = STDIN.gets.chomp()
 
+##se File.open() to open the file by provided filename again
 txt_again = File.open(file_again)
 
+
+#Show file content by read() again
 puts txt_again.read()
