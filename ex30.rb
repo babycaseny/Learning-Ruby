fe853fc270019cff64c6d35b4a2c9d8cb8e41cc1
Sardo Ip <sardo.ip@nxtomo.hk>
2014-12-29 17:41:59 +0800

    Exercise 30: Else and If

---
+++ ex30.rb
@@ -0,0 +1,34 @@
+people = 50
+cars = 50
+trucks = 40
+
+if cars > people
+    puts "We should take the cars."
+
+elsif cars < people
+    puts "We should not take the cars."
+
+else
+    puts "We can't decide."
+
+end
+
+if trucks > cars
+    puts "That's too many trucks."
+
+elsif trucks <= cars and cars == people
+    puts "Cars is enough than trucks."
+
+elsif trucks < cars
+    puts "Maybe we could take the trucks."
+
+else
+    puts"We still can't decide."
+
+end
+
+if people > trucks
+    puts "Alright, let's just take the trucks."
+else
+    puts "Fine,let's stay home then."
+end
