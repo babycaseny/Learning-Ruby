799c4ad37a717109487ca5e830fef316979bb78c
Sardo Ip <sardo.ip@nxtomo.hk>
2014-12-30 11:31:10 +0800

    Exercise 34: Accessing Elements of Arrays

---
+++ ex34.rb
@@ -0,0 +1,13 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+kancolle = ['Zuikaku','Shokaku','Akagi','Kaga','Hiei','Kongo']
+
+zuikaku = kancolle[0]
+shokaku = kancolle[1]
+akagi = kancolle[2]
+kaga = kancolle[3]
+hiei = kancolle[4]
+kongo = kancolle[5]
+
+puts zuikaku,shokaku,akagi,kaga,hiei,kongo
