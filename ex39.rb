4ec584776bcf073fbd58506b2b6474db2ad2d124
Sardo Ip <sardo.ip@nxtomo.hk>
2014-12-31 11:53:09 +0800

    Rename file

--- ex39.rb
+++ ex39.rb
@@ -1,30 +1,31 @@
-ten_things = "Apples Oranges Crows Telephone Light Sugar"
+cities = {'CA' => 'San Francisco',
+          'MI' => 'Detroit',
+          'FL' => 'Jacksonville'}
 
-puts "Wait there's not 10 things in that list, let's fix that."
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
 
-stuff = ten_things.split(' ')
-more_stuff = %w(Day Night Song Frisbee Corn Banana Girl Boy)
-
-while stuff.length != 10
-    next_one = more_stuff.pop()
-    puts "Adding: #{next_one}"
-    stuff.push(next_one)
-    puts "There's #{stuff.length} items now."
+def find_city(map, state)
+    if map.include? state
+        return map[state]
+    else
+        return "Not Found."
+    end
 end
 
-while stuff.length != 10
-    next_one = more_stuff.pop()
-    puts "Adding #{next_one}"
-    stuff.push(next_one)
-    puts "There's #{stuff.length} items now."
+#ok pay attention!
+cities[:find] = method(:find_city)
+
+while true
+    print "State? (ENTER to quit)"
+    state = gets.chomp
+
+    break if state.empty?
+
+    #this line is the most improtant ever! study!
+    puts cities[:find].call(cities, state)
+
+    for i in cities
+        puts i
+    end
 end
-
-puts "There we go: #{stuff}"
-
-puts "Let's do some things with stuff."
-
-puts stuff[1]
-puts stuff[-1] #whoa! fancy
-puts stuff.pop()
-puts stuff.join(' ')# what? cool!
-puts stuff.values_at(3,5).join('#') # super stellar!
