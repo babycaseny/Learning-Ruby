8eeb357d427e8a89be1909a7d33db9aceff01cbc
babycaseny <babycaseny@gmail.com>
2015-01-26 18:36:56 +0800

    Update KùstomaisenSorting.rb

--- KùstomaisenSorting.rb
+++ KùstomaisenSorting.rb
@@ -1,50 +1,41 @@
-## Riförènsing websàits:
-## * http://ubuntuforums.org/showthread.php?t=523273
-## * http://stackoverflow.com/questions/15401061/custom-sort-method-in-ruby
-## * http://stackoverflow.com/questions/4622206/ruby-how-to-pass-a-custom-comparator-to-sort
+## Riförènsing websàits in kontent.
 
 def splitString(input)
   # Rif: http://ubuntuforums.org/showthread.php?t=523273
   return input.scan(/./)
 end 
 
-def 
-  # Rif: http://stackoverflow.com/questions/15401061/custom-sort-method-in-ruby
-
-def 
-  ar.sort do |a, b|
-  case
-  when a.x < b.x
-    -1
-  when a.x > b.x
-    1
-  else
-    a.y <=> b.y
-  end
-end 
-
-class SizeMatters
+class BabyishString
 # Rif: http://stackoverflow.com/questions/4622206/ruby-how-to-pass-a-custom-comparator-to-sort
   include Comparable
   attr :str
+
   def <=>(anOther)
-    str.size <=> anOther.str.size
+  # Rif: http://stackoverflow.com/questions/15401061/custom-sort-method-in-ruby
+  a, b = str[0], anOther.str[0]
+  case
+  when a < b
+    -1
+  when a > b
+    1
+  else
+    # http://stackoverflow.com/questions/3614389/what-is-the-easiest-way-to-remove-the-first-character-from-a-string
+    return str[1..-1] <=> anOther.str[1..-1]
   end
+
   def initialize(str)
     @str = str
   end
+
   def inspect
     @str
   end
 end
 
-s1 = SizeMatters.new("Z")
-s2 = SizeMatters.new("YY")
-s3 = SizeMatters.new("XXX")
-s4 = SizeMatters.new("WWWW")
-s5 = SizeMatters.new("VVVVV")
+bbText1, bbText2, bbText3 = BabyishString.new
+bbText1 = "BB-wa tincoi!"
+bbText2 = "Muzài-wa dùkyi da."
+bbText3 = "Yöngmezài"
 
-s1 < s2                       #=> true
-s4.between?(s1, s3)           #=> false
-s4.between?(s3, s5)           #=> true
-[ s3, s2, s5, s4, s1 ].sort   #=> [Z, YY, XXX, WWWW, VVVVV]
+puts bbText1 < bbText2
+puts bbText1 > bbText2
