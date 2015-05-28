65d44f07053633f12b7737d3577014081b5d8b8f
Tom K. C. Chiu <babycaseny@gmail.com>
2015-05-28 19:01:22 +0800

    Remove wordcounted module as not functioning

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'https://rubygems.org'
 
 gem 'bson', '~> 3.0'
 gem 'mongo', '~> 2.0'
-
+gem 'words_counted'
--- countingWordsBenchmark.rb
+++ countingWordsBenchmark.rb
@@ -7,17 +7,8 @@ require 'mongo'
 # without having to specify the Mongo namespace.
 include Mongo
 
-# See: https://github.com/abitdodgy/words_counted
-gem 'words_counted'
-
-# A MongoClient instance represents a connection to MongoDB.
-# mongo_client = MongoClient.new("MW-GAMP103240", 60001)
-# Using a DB
-# db = mongo_client.db('benchmark')
-# auth = db.authenticate(my_user_name, my_password)
-db = Mongo::Client.new('mongodb://MW-GAMP103240:60001/benchmark')
-
-posts = db.collection('posts')
+# Syntax: http://docs.mongodb.org/ecosystem/tutorial/ruby-driver-tutorial/#ruby-driver-tutorial
+mongo_client = Mongo::Client.new([ 'MW-GAMP103240:60001' ], :database => 'benchmark', :connect => :sharded)
 
 charSet = 'AaĀāÁáǍǎÀàÄäǞǟÄ́ä́Ä̌ä̌Ä̀ä̀BbCcÇçČčĈĉDdĐđEeĒēÉéĚěÈèFfGgĜĝ' +
 	  'HhĤĥIiĪīÍíǏǐÌìJjKkLlMmM̄m̄ḾḿM̌m̌M̀m̀NnN̄n̄ŃńŇňǸǹŊŋŊ̄ŋ̄' +
@@ -30,30 +21,28 @@ puts "Nei supplaied fayl: #{filename}:"
 lines, words, chars = 0
 freqData = nil
 File.open(filename) do |hFile|
-	hFile.each_line do |line|
-	lines += 1
-	# chars += strlen(line)
-	# words = str_word_count(line, 1, charSet)
-	# puts words
-	# words += count(words)
-	counter = WordsCounted.count(line, regexp: /[\p{Alnum}\-']+/)
-	words += counter.word_count
-	chars += counter.char_count
-	counter.word_occurrences.each_key do |key|
-		# occurrence: "we"      => 1
-		# put it into MongoDB
-		post = Posts.find( :_id => key ).first
-		if post
-			post[:author] = "Ethan Gunderson"
-			posts.update( { :_id => post_id }, post )
-		else
-			new_post = { :title => "RubyLearning.com, its awesome", 
-			     :content => "This is a pretty sweet way to learn Ruby", 
- 			     :created_on => Time.now }
-			post_id = posts.insert(new_post)
-		end
-	end
-	end
+    hFile.each_line do |line|
+        lines += 1
+        chars += strlen(line)
+        words = str_word_count(line, 1, charSet)
+        puts words
+        words += count(words)
+        counter.word_occurrences.each_key do |key|
+            puts key
+            # occurrence: "we"      => 1
+            # put it into MongoDB
+            post = mongo_client.find( :_id => key ).first
+            if post
+                post[:author] = "Ethan Gunderson"
+                mongo_client.update( { :_id => post_id }, post )
+            else
+                new_post = { :title => "RubyLearning.com, its awesome", 
+		             :content => "This is a pretty sweet way to learn Ruby", 
+                             :created_on => Time.now }
+                post_id = mongo_client.insert(new_post)
+             end
+        end
+    end
 end
 
 hFile.close
